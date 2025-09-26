159d24ffac9b4b1a5e41fe530ec591666a98301f
zzaitun <zet.zaitun@gmail.com>
2025-09-26 16:23:55 +0800

    Mengubah Data

---
+++ praktikum-1.py
@@ -0,0 +1,68 @@
+print("Praktikum Pengantar Pemrograman")
+print()
+print('Nama Lengkap  : Mahasiswa')
+print('NIM           : 6002201014')
+print('Prodi         : Matematika')
+
+angkatan = 2025
+print("angkatan adalah",angkatan)
+print()
+
+print()
+kampus = "Institut Teknologi BJ Habibie"
+print("Data yang dibuat adalah",kampus)
+print("Tipe datanya adalah",type(kampus))
+
+print()
+x = 22
+print("Data yang dibuat adalah",x)
+print("Tipe datanya adalah",type(x))
+
+print()
+a = 22.2
+print("Data yang dibuat adalah",a)
+print("Tipe datanya adalah",type(a))
+
+
+waktu = 137
+
+menit = waktu//60
+
+detik = waktu%60
+
+# Nilai 137 detik sama dengan 2 menit 17 detik
+
+print("Nilai",waktu,"detik sama dengan",menit,"menit",detik,"detik")
+
+print()
+a = 4
+print("nilai a sebelum adalah",a)
+
+print()
+a+=3 # a = a + 3
+print("nilai a setelah a+=3 adalah",a)
+
+print()
+a-=5 # a = a - 5
+print("nilai a setelah a-=5 adalah",a)
+
+print()
+a*=5 # a = a * 5
+print("nilai a setelah a*=5 adalah",a)
+
+print()
+a/=2 # a = a / 2
+print("nilai a setelah a/=2 adalah",a)
+
+print()
+a**=2 # a = a ** 2
+print("nilai a setelah a**=2 adalah",a)
+
+print()
+a%=7 # a = a % 7
+print("nilai a setelah a%=7 adalah",a)
+
+print()
+a//=3 # a = a // 3
+print("nilai a setelah a//=3 adalah",a)
+
